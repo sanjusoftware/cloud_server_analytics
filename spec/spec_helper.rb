a30c06a2a31029a79a5f04629efdee29907cd70a
Sanjeev Mishra <sanjeev@codemancers.com>
2012-05-13 16:43:17 +0530

    added testing framework now

---
+++ spec/spec_helper.rb
@@ -0,0 +1,12 @@
+__LIB_DIR__ = File.expand_path(File.join(File.dirname(__FILE__), "..","lib"))
+
+$LOAD_PATH.unshift __LIB_DIR__ unless
+  $LOAD_PATH.include?(__LIB_DIR__) ||
+  $LOAD_PATH.include?(File.expand_path(__LIB_DIR__))
+
+require 'cloud_server_analytics'
+require 'mocha'
+
+RSpec.configure do |config|
+  config.mock_framework = :mocha
+end
